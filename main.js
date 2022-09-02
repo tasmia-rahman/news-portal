0bc41de418aca1a9d3df8e2c59c70cc8a8caabb6
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-09-02 22:40:45 +0600

    display categories part done

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
-        .then(data => console.log(data.data.news_category))
+        .then(data => displayCategories(data.data.news_category))
         .catch(error => console.log(error))
 
 }
 loadCategories();
 
+const displayCategories = (categories) => {
+    const categoriesContainer = document.getElementById('categories-container');
+    categories.forEach(category => {
+        const li = document.createElement('li');
+        li.classList.add('nav-link')
+        li.innerText = category.category_name;
+        categoriesContainer.appendChild(li);
+    });
+
+}
